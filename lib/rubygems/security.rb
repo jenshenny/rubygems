6e662cc8b564e3970abf604b800b904cc927fcbe
Frederik Dudzik <5946811+doodzik@users.noreply.github.com>
2021-10-19 10:46:51 -0700

    Apply suggestions from code review

--- lib/rubygems/security.rb
+++ lib/rubygems/security.rb
@@ -435,7 +435,7 @@ module Gem::Security
   # Checks if the provided key is private.
   # In Ruby 2.3 EC doesn't implments the private_key? but not the private? method
 
-  OpenSSL::PKey::EC.send(:alias_method, :private?, :private_key?) if defined?(OpenSSL::PKey::EC)
+  OpenSSL::PKey::EC.send(:alias_method, :private?, :private_key?) if defined?(OpenSSL::PKey::EC) && Gem::Version.new(Gem::VERSION) < Gem::Version.new("2.4.0")
 
   ##
   # Creates a self-signed certificate with an issuer and subject from +email+,
